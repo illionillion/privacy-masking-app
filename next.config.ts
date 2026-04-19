0af9bdbc07028302ae1527bea1e27b9808603e8c
yuto ichinose <yuto.0526.sano@gmail.com>
2026-04-19 20:07:52 +0900

    ci: 静的エクスポートとGitHub PagesデプロイのCD設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: process.env.NODE_ENV === "production" ? "docs" : ".next",
+  cleanDistDir: false,
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
